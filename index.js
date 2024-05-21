fb7a7b75fdeb5494fc4b83bceb8eb1c70c925e15
Tim Atobatele <dev.atobatele@gmail.com>
2024-05-21 18:51:02 +0000

    URL Shortener : 2 / 4 Tests Passing

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const dns = require('dns')
+const bodyParser = require('body-parser');
+const { error } = require('console');
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -13,12 +15,26 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.post('/api/shorturl', function(req, res) {
+  let {url} = req.body;
+  let resObj = {
+    original_url: url,
+    short_url:'short'
+  };
+  dns.lookup(url,function (err) {
+    if (err.code=='ENOTFOUND') {
+      resObj = {error:'invalid url'}
+    }
+  })
 
+  res.json(resObj);
+});
+/*app.get('api/shorturl',function (req,res) {
+  
+})*/
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });

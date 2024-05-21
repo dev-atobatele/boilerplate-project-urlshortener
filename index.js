9a8da6c5d677b45cf7558061b49feda05d6c99dc
Tim Atobatele <dev.atobatele@gmail.com>
2024-05-21 22:06:13 +0000

    URL Shortener : Complete

--- index.js
+++ index.js
@@ -19,21 +19,28 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 // Your first API endpoint
 let counter = 0;
+let uri = ''
+
 app.post('/api/shorturl', function(req, res) {
   let url = new URL(req.body.url);
-  console.log(url.href)
   let resObj = {
     original_url: url.href,
-    short_url:counter
+    short_url: counter
   };
-  if(url.protocol != 'https:'){resObj = {error:'invalid url'}}
-  console.log(resObj)
+  if(url.protocol != 'https:'){
+    resObj = {error:'invalid url'}
+    console.log(url.href);
+  }
   res.json(resObj);
   counter += 1;
+  uri = url.href;
+  console.log(resObj);
 });
-/*app.get('api/shorturl',function (req,res) {
-  
-})*/
+
+app.get(`/api/shorturl/:${counter}`,function (req,res) {
+  res.redirect(uri);
+  console.log(`${req.url} Will redirect to : ${uri}`);
+});
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
